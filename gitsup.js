21d29f744cdc85e3c37c22a4f81c4014dd5f923e
Howard Roark <howie@codingcoop.org>
2016-03-08 11:21:02 -0500

    Testing out adding votes.

--- gitsup.js
+++ gitsup.js
@@ -24,30 +24,39 @@ if (Meteor.isClient) {
         $('title').text(username+'/'+repository+'/'+listType+' · gitsup')
         $('h2').html('<a href="https://github.com/'+username+'/'+repository+'/'+listType+'">'+username+'/'+repository+'/'+listType+'</a>')
 
-        $.get('https://api.github.com/repos/'+username+'/'+repository+'/'+listType, function(data) {
-            console.log(data)
-            $('ol').empty()
-            for (i = 0; i < data.length; i++) {
-              $('ol').append(
-                '<li>'+
-                  '<h3>'+
-                    '<a href="#'+data[i].number+'"><img src="/vote.gif" alt="Vote" /></a> '+
-                    '<a href="'+data[i].html_url+'">'+data[i].title+'</a> '+
-                    '<span>(<a href="'+data[i].html_url+'">#'+data[i].number+'</a>)</span>'+
-                  '</h3>'+
-                  '<p>'+
-                    '<span>'+
-                      '3 points by'+
-                    '</span> '+
-                    '<a href="'+data[i].user.html_url+'">'+data[i].user.login+'</a> '+
-                    '<span>'+
-                      '2 hours ago |'+
-                    '</span> '+
-                    '<a href="'+data[i].comments_url+'">'+data[i].comments+' comments</a>'+
-                  '</p>'+
-                '</li>'
-              )
-            }
+        var repoId;
+        $.get('https://api.github.com/repos/'+username+'/'+repository, function(data) {
+            repoId = data.id
+            $.get('https://api.github.com/repos/'+username+'/'+repository+'/'+listType, function(data) {
+                $('ol').empty()
+                for (i = 0; i < data.length; i++) {
+                  $('ol').append(
+                    '<li>'+
+                      '<h3>'+
+                        '<a href="#'+data[i].number+'" class="vote" data-issue-id="'+data[i].id+'"><img src="/vote.gif" alt="Vote" /></a> '+
+                        '<a href="'+data[i].html_url+'">'+data[i].title+'</a> '+
+                        '<span>(<a href="'+data[i].html_url+'">#'+data[i].number+'</a>)</span>'+
+                      '</h3>'+
+                      '<p>'+
+                        '<span>'+
+                          '3 points by'+
+                        '</span> '+
+                        '<a href="'+data[i].user.html_url+'">'+data[i].user.login+'</a> '+
+                        '<span>'+
+                          '2 hours ago |'+
+                        '</span> '+
+                        '<a href="'+data[i].comments_url+'">'+data[i].comments+' comments</a>'+
+                      '</p>'+
+                    '</li>'
+                  )
+                  $($('ol li .vote')[i]).click(function(e){
+                      var issueId = $(e.currentTarget).data('issueId')
+                      Votes.insert({repoId:repoId, issueId:issueId})
+                      console.log(repoId, issueId)
+                      return false
+                  })
+                }
+            })
         })
     })
 }
