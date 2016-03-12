9ac3714fad04c8e2f9160a1c7e27ffa7e8cea0d3
Marios Antonoudiou <marios.antonoudiou@gmail.com>
2016-03-12 20:52:07 +0200

    Modify issues route to use the same template

--- client/components/pullsIndex/pullsIndex.js
+++
@@ -1,5 +0,0 @@
-Template.pullsIndex.helpers({
-  example: function() {
-    return 'https://github.com/' + this.user + '/' + this.project + '/issues';
-  }
-});
--- routes/routes.js
+++ routes/routes.js
@@ -6,24 +6,28 @@ Router.route('/', function() {
   this.render('home');
 });
 
-Router.route('/:user/:project/issues', function() {
-  this.render('issuesIndex', {
-    data: function() {
-      return {
-        user: this.params.user,
-        project: this.params.project
-      }
-    }
-  });
-});
+/* Issues route
+*
+*  /user/project/
+*  /user/project/issues
+*  /user/project/pulls
+*
+*  It passes view user, project and filter parameters. Filter
+*  parameter can be one of "issues", "pulls" or "all".
+*/
+_.each(['issues', 'pulls', ''], function(filter) {
+  var issuesRoute = '/:user/:project/' + filter;
 
-Router.route('/:user/:project/pulls', function() {
-  this.render('pullsIndex', {
-    data: function() {
-      return {
-        user: this.params.user,
-        project: this.params.project
+  Router.route(issuesRoute, function() {
+    this.render('issuesIndex', {
+      data: function() {
+        return {
+          user: this.params.user,
+          project: this.params.project,
+          filter: filter || 'all'
+        }
       }
-    }
+    });
   });
+
 });
