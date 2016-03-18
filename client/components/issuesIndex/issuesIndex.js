34c5f6e43e0be35f42b12e209b30b993374164be
Marios Antonoudiou <marios.antonoudiou@gmail.com>
2016-03-19 00:20:42 +0200

    Add github object

--- client/components/issuesIndex/issuesIndex.js
+++ client/components/issuesIndex/issuesIndex.js
@@ -1,5 +1,10 @@
 Template.issuesIndex.helpers({
   example: function() {
     return 'https://github.com/' + this.user + '/' + this.project + '/issues';
+  },
+  issues: function() {
+    var user = this.user;
+    var project = this.project;
+    
   }
 });
