8461183db9ecac40406967c1529317a386d12d25
Marios Antonoudiou <marios.antonoudiou@gmail.com>
2016-03-20 17:05:59 +0200

    Add issues and test

--- tests/client/github.js
+++ tests/client/github.js
@@ -1,6 +1,10 @@
 var _ = require('underscore');
 
+
 describe('Github object', function() {
+  beforeEach(function() {
+  });
+
   it('.endpoint should be https://api.github.com @watch', function () {
     browser.url('http://localhost:3000');
     var ret = browser.execute(function() {return Github.endpoint});
@@ -8,19 +12,16 @@ describe('Github object', function() {
   });
 
   it('.userAgent should be support@gitsup.com @watch', function () {
-    browser.url('http://localhost:3000');
     var ret = browser.execute(function() {return Github.userAgent});
     expect(ret.value).to.equal('support@gitsup.com');
   });
 
-  it('.issues should fetch Github issues @watch', function (done) {
-    browser.url('http://localhost:3000');
-    var ret = browser.executeAsync(function(done) {
-      Github.issues('gitsup', 'gitsup', function(res) { done(res); });
+  it('.issues should fetch Github issues @watch', function () {
+    var githubResponse = browser.executeAsync(function(next) {
+      Github.issues('gitsup', 'gitsup', function(res) {
+        next(res);
+      })
     });
-    expect(typeof ret.value).to.equal('object')
+    expect(typeof githubResponse).to.equal('object')
   });
-
-
-
 });
