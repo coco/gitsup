b5fa6fc95a7d893e7fca2baa1f726fdfea6cc690
Howard Roark <howie@codingcoop.org>
2016-06-12 09:46:27 -0700

    Fixing multiple update issue.

--- app.js
+++ app.js
@@ -1,4 +1,59 @@
-var Issues = new Mongo.Collection("issues")
+Issues = new Mongo.Collection("issues")
+
+if (Meteor.isClient) {
+    var parser = document.createElement('a')
+    parser.href = document.location.href
+    var path = parser.pathname.split('/')
+
+    var userName = path[1]
+    var projectName = path[2]
+
+    var state = { userName:userName, projectName:projectName }
+
+    Meteor.call('syncIssues', state, function(err, response) {
+        if(err) {
+            Session.set('serverDataResponse', "Error:" + err.reason)
+            return
+        }
+    })
+
+    var ITEMS_INCREMENT = 30
+    Session.setDefault('itemsLimit', ITEMS_INCREMENT)
+    state.limit = Session.get('itemsLimit')
+    Deps.autorun(function() {
+        Meteor.subscribe('items', state)
+    })
+
+    Template.issues.helpers({
+        items: function() {
+            return Issues.find({})
+        },
+        moreResults: function() {
+            return !(Issues.find().count() < Session.get("itemsLimit"))
+        }
+    })
+ 
+    function showMoreVisible() {
+        var threshold, target = $("#showMoreResults")
+        if (!target.length) return
+     
+        threshold = $(window).scrollTop() + $(window).height() - target.height()
+     
+        if (target.offset().top < threshold) {
+            if (!target.data("visible")) {
+                target.data("visible", true)
+                Session.set("itemsLimit",
+                    Session.get("itemsLimit") + ITEMS_INCREMENT)
+            }
+        } else {
+            if (target.data("visible")) {
+                target.data("visible", false)
+            }
+        }        
+    }
+ 
+    $(window).scroll(showMoreVisible)
+}
 
 if (Meteor.isServer) {
     Meteor.startup(function () {
@@ -31,7 +86,7 @@ if (Meteor.isServer) {
                         issue.userName = userName
                         issue.projectName = projectName
 
-                        var existingIssue = Issues.find({id: issue.id}).fetch[0]
+                        var existingIssue = Issues.find({id: issue.id}).fetch()[0]
 
                         if(typeof existingIssue != 'undefined') {
                             Issues.update({id: issue.id}, {$set: issue})
@@ -46,64 +101,8 @@ if (Meteor.isServer) {
             }
         })
 
-        Meteor.publish('issues', function(limit){
-            return Issues.find({},{limit:limit})
+        Meteor.publish('issues', function(state){
+            return Issues.find({userName: state.userName, projectName: state.projectName},{limit:state.limit})
         })
     })
-}
-
-if (Meteor.isClient) {
-    var parser = document.createElement('a')
-    parser.href = document.location.href
-    var path = parser.pathname.split('/')
-
-    var userName = path[1]
-    var projectName = path[2]
-
-    var state = { userName:userName, projectName:projectName }
-
-    console.log(state)
-
-    Meteor.call('syncIssues', state, function(err, response) {
-        if(err) {
-            Session.set('serverDataResponse', "Error:" + err.reason)
-            return
-        }
-    })
-
-    var ITEMS_INCREMENT = 30
-    Session.setDefault('itemsLimit', ITEMS_INCREMENT)
-    Deps.autorun(function() {
-        Meteor.subscribe('items', Session.get('itemsLimit'))
-    })
-
-    Template.issues.helpers({
-        items: function() {
-            return Issues.find({})
-        },
-        moreResults: function() {
-            return !(Issues.find().count() < Session.get("itemsLimit"))
-        }
-    })
- 
-    function showMoreVisible() {
-        var threshold, target = $("#showMoreResults")
-        if (!target.length) return
-     
-        threshold = $(window).scrollTop() + $(window).height() - target.height()
-     
-        if (target.offset().top < threshold) {
-            if (!target.data("visible")) {
-                target.data("visible", true)
-                Session.set("itemsLimit",
-                    Session.get("itemsLimit") + ITEMS_INCREMENT)
-            }
-        } else {
-            if (target.data("visible")) {
-                target.data("visible", false)
-            }
-        }        
-    }
- 
-    $(window).scroll(showMoreVisible)
 }
